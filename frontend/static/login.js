f5e8a858e9f51e86c14edd95b2b7d6c270dd7777
BronyLong <egorp.2003@mail.ru>
2024-01-30 15:30:02 +0300

    + return values of login and password fields as json format

---
+++ frontend/static/login.js
@@ -0,0 +1,20 @@
+var login = document.getElementById('login')
+var formElement = document.getElementById('loginForm');
+
+login.addEventListener('click', event => {
+    event.preventDefault();
+    console.log("Clicked");
+    const formData = new FormData(formElement);
+
+    if (formData.get('password') == '') {
+        alert('Вы не ввели пароль');
+    }
+    else {
+        const formData = new FormData(formElement);
+        const data = Object.fromEntries(formData);
+
+        console.log(data);
+    }
+});
+
+
